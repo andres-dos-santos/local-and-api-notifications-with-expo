f736f38a6ca404146322c812c31bb440a7f437e5
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-05-09 21:14:56 -0300

    feat(App.tsx): create local and api notifications

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+const PROJECT_ID = Constants.expoConfig?.extra?.eas.projectId;
 
 export default function App() {
+  const handleCallNotifications = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Without permissions.");
+
+      return;
+    }
+
+    /** await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Hello.",
+        body: "Notification is here!",
+      },
+      trigger: {
+        seconds: 5,
+      },
+    }); */
+
+    const token = await Notifications.getExpoPushTokenAsync({
+      projectId: PROJECT_ID,
+    });
+
+    console.log(token);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Open up App.tsx to start working on your app!
+      </Text>
+
+      <Button title="Notificar" onPress={handleCallNotifications} />
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +56,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1c1c1c",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    marginBottom: 20,
   },
 });
